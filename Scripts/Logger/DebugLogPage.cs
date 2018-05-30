833c2c1a60f971f1d1b7235023dccb29be3ce4c7
Harry Hodgson <harry.hodgson@dubitlimited.com>
2018-05-30 11:41:21 +0100

    Logger improvements

--- Scripts/Logger/DebugLogPage.cs
+++ Scripts/Logger/DebugLogPage.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace DUCK.DebugMenu
+namespace DUCK.DebugMenu.Logger
 {
 	/// <summary>
 	/// Manages a list of log entries displayed in the debug menu
 	/// </summary>
-	public class DebugMenuLog : MonoBehaviour
+	public class DebugLogPage : MonoBehaviour
 	{
-		private static readonly Dictionary<LogType, Color> logColors = new Dictionary<LogType, Color>
-		{
-			{LogType.Default, new Color(185, 185, 185)},
-			{LogType.Warning, new Color(255, 165, 0)},
-			{LogType.Error, new Color(255, 0, 0)}
-		};
-
 		private enum LogType
 		{
 			Default = 12171705,
 			Warning = 16753920,
 			Error = 16711680
+		}
+
+		private class LogTypeConfig
+		{
+			public Color Color { get; private set; }
+			public Sprite Icon { get; private set; }
+
+			public LogTypeConfig(Color color, Sprite icon)
+			{
+				Color = color;
+				Icon = icon;
+			}
 		}
 
 		private struct PendingLog
@@ -36,26 +41,39 @@ namespace DUCK.DebugMenu
 			}
 		}
 
+		[Header("Log Type Icons")]
+		[SerializeField]
+		private Sprite infoIcon;
+		[SerializeField]
+		private Sprite warningIcon;
+		[SerializeField]
+		private Sprite errorIcon;
+		[Header("Components")]
 		[SerializeField]
 		private ScrollRect scrollRect;
-
 		[SerializeField]
 		private RectTransform container;
-
 		[SerializeField]
-		private RectTransform entryPrefab;
-
+		private LogEntryElement entryPrefab;
 		[SerializeField]
 		private Button clearButton;
 
 		private Queue<PendingLog> pendingLogs;
+		private Dictionary<LogType, LogTypeConfig> logTypeConfigs;
 
-		private static DebugMenuLog instance;
+		private static DebugLogPage instance;
 
 		private void Awake()
 		{
 			entryPrefab.gameObject.SetActive(false);
 			clearButton.interactable = false;
+
+			logTypeConfigs = new Dictionary<LogType, LogTypeConfig>
+			{
+				{LogType.Default, new LogTypeConfig(new Color(185, 185, 185), infoIcon)},
+				{LogType.Warning, new LogTypeConfig(new Color(255, 165, 0), warningIcon)},
+				{LogType.Error, new LogTypeConfig(new Color(255, 0, 0), errorIcon)}
+			};
 
 			Initialise();
 		}
@@ -120,15 +138,12 @@ namespace DUCK.DebugMenu
 		{
 			clearButton.interactable = true;
 
+			var logTypeConfig = logTypeConfigs[logType];
+
 			var newLogEntry = Instantiate(entryPrefab, container, false);
 			newLogEntry.transform.SetAsLastSibling();
-
-			var textComponent = newLogEntry.GetComponent<Text>();
-			textComponent.text = text;
-
-			var iconComponent = newLogEntry.GetComponentInChildren<Image>();
-			iconComponent.color = logColors[logType];
-
+			newLogEntry.TextComponent.text = text;
+			newLogEntry.ImageComponent.sprite = logTypeConfig.Icon;
 			newLogEntry.gameObject.SetActive(true);
 		}
 
---
+++ Scripts/Logger/LogEntryElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DUCK.DebugMenu.Logger
+{
+	public class LogEntryElement : MonoBehaviour
+	{
+		public Text TextComponent { get { return text; } }
+		public Image ImageComponent { get { return icon; } }
+
+		[SerializeField]
+		private Image icon;
+		[SerializeField]
+		private Text text;
+	}
+}
