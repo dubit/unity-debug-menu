d003b4d4e5b64890098b240474b7cda99c365898
Harry Hodgson <harry.hodgson@dubitlimited.com>
2018-05-30 11:41:00 +0100

    Cloud build info page skinning and deserialised to display

---
+++ Scripts/CloudBuild/CloudBuildManifest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace DUCK.DebugMenu.CloudBuild
+{
+	public class CloudBuildManifest
+	{
+		public string ScmCommitId { get { return scmCommitId; } }
+		public string ScmBranch { get { return scmBranch; } }
+		public string BuildNumber { get { return buildNumber; } }
+		public string BuildStartTime { get { return buildStartTime; } }
+		public string ProjectId { get { return projectId; } }
+		public string BundleId { get { return bundleId; } }
+		public string UnityVersion { get { return unityVersion; } }
+		public string XcodeVersion { get { return xcodeVersion; } }
+		public string CloudBuildTargetName { get { return cloudBuildTargetName; } }
+		
+		[SerializeField]
+		private string scmCommitId;
+		[SerializeField]
+		private string scmBranch;
+		[SerializeField]
+		private string buildNumber;
+		[SerializeField]
+		private string buildStartTime;
+		[SerializeField]
+		private string projectId;
+		[SerializeField]
+		private string bundleId;
+		[SerializeField]
+		private string unityVersion;
+		[SerializeField]
+		private string xcodeVersion;
+		[SerializeField]
+		private string cloudBuildTargetName;
+	}
+}
---
+++ Scripts/CloudBuild/CloudBuildPage.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DUCK.DebugMenu.CloudBuild
+{
+	public class CloudBuildPage : MonoBehaviour
+	{
+		[Header("Cloud Build Manifest Info Text")]
+		[SerializeField]
+		private Text scmCommitId;
+		[SerializeField]
+		private Text scmBranch;
+		[SerializeField]
+		private Text buildNumber;
+		[SerializeField]
+		private Text buildStartTime;
+		[SerializeField]
+		private Text projectId;
+		[SerializeField]
+		private Text bundleId;
+		[SerializeField]
+		private Text unityVersion;
+		[SerializeField]
+		private Text xcodeVersion;
+		[SerializeField]
+		private Text cloudBuildTargetName;
+
+		public void Awake()
+		{
+			var buildManifestJson = Resources.Load<TextAsset>("UnityCloudBuildManifest.json");
+			if (!buildManifestJson) return;
+
+			var buildManifest = JsonUtility.FromJson<CloudBuildManifest>(buildManifestJson.text);
+			if (buildManifest != null)
+			{
+				scmCommitId.text = buildManifest.ScmCommitId;
+				scmBranch.text = buildManifest.ScmBranch;
+				buildNumber.text = buildManifest.BuildNumber;
+				buildStartTime.text = buildManifest.BuildStartTime;
+				projectId.text = buildManifest.ProjectId;
+				bundleId.text = buildManifest.BundleId;
+				unityVersion.text = buildManifest.UnityVersion;
+				xcodeVersion.text = buildManifest.XcodeVersion;
+				cloudBuildTargetName.text = buildManifest.CloudBuildTargetName;
+#if !UNITY_IPHONE
+				xcodeVersion.gameObject.SetActive(false);
+#endif
+			}
+		}
+	}
+}
