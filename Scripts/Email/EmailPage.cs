1364ea25861024c9edcc13cc13e55599998c0341
Harry Hodgson <harry.hodgson@dubitlimited.com>
2018-05-31 13:14:34 +0100

    Send email

--- Scripts/Email/EmailPage.cs
+++ Scripts/Email/EmailPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,8 @@ namespace DUCK.DebugMenu.Email
 	{
 		[SerializeField]
 		private Button backButton;
+		[SerializeField]
+		private Button sendEmailButton;
 		[SerializeField]
 		private Dropdown emailDropdown;
 		[SerializeField]
@@ -21,7 +24,7 @@ namespace DUCK.DebugMenu.Email
 		private Image bodyImage;
 
 		private Action onBack;
-		
+
 		private string[] emailAddresses;
 
 		private void Awake()
@@ -30,12 +33,14 @@ namespace DUCK.DebugMenu.Email
 			emailAddresses = emailAddressesModel.emails;
 
 			emailDropdown.AddOptions(emailAddresses.ToList());
-			
+
 			backButton.onClick.AddListener(() =>
 			{
 				gameObject.SetActive(false);
 				onBack();
 			});
+
+			sendEmailButton.onClick.AddListener(SendEmail);
 		}
 
 		public void Show(string subject, string body, Action onBack)
@@ -45,10 +50,19 @@ namespace DUCK.DebugMenu.Email
 			subjectText.text = subject;
 			bodyText.text = body;
 		}
-		
+
 		public void Show(string subject, Texture body)
 		{
-			
+		}
+
+		private void SendEmail()
+		{
+			var email = emailDropdown.options[emailDropdown.value].text;
+
+			var body = new StringBuilder();
+			body.Append(bodyText.text);
+			var mailTo = string.Format("mailto:{0}?subject={1}&body={2}", email, subjectText.text, bodyText.text);
+			Application.OpenURL(mailTo);
 		}
 	}
 }
